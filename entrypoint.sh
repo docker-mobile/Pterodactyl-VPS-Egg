ca3a8f9dbe81b8dd685bf3b975237b944ad654b8
docker-mobile <143889618+docker-mobile@users.noreply.github.com>
2024-11-16 09:07:48 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 sleep 2
-export HOME=/home/container
-cd /home/container
+cd ~
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Copy run.sh to /home/container
-cp /run.sh "$HOME/run.sh"
+wget https://github.com/docker-mobile/Pterodactyl-VPS-Egg/raw/refs/heads/main/run.sh
+cp ./run.sh "~/run.sh"
 
 # Make run.sh executable.
-chmod +x "$HOME/run.sh"
+chmod +x "~/run.sh"
 
 # Run the VPS Installer
-bash /install.sh
+wget https://github.com/docker-mobile/Pterodactyl-VPS-Egg/raw/refs/heads/main/install.sh
+bash ./install.sh
